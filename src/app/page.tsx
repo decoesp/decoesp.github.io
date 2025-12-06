5cdb5cd9396cb1fad11f5dd65002300c8433146b
André Filho <decoesp@gmail.com>
2025-12-06 17:22:53 -0300

    fix: resolve errors on build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,6 @@ import { Card } from '@/components/card'
 export default function Home() {
   const pages = getAllPages()
   const aboutPage = pages.find((page) => page.slug === 'sobre')
-  const contactPage = pages.find((page) => page.slug === 'contato')
-  const freelaPage = pages.find((page) => page.slug === 'trabalhos')
 
   const experiences = getAllExperiences()
   const projects = getAllProjects().filter((p) => p.featured)
@@ -14,8 +12,6 @@ export default function Home() {
   return (
     <Card
       aboutContent={aboutPage?.content}
-      contactContent={contactPage?.content}
-      freelaContent={freelaPage?.content}
       experiences={experiences}
       projects={projects}
       posts={recentPosts}
